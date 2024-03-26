bb149449cc1143f2d92d0695c981745901b36d13
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2024-03-26 20:32:18 +0300

    CharController3
    Jump and gravity added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,21 @@ public class PlayerMovement : MonoBehaviour
     // Camera Controller
     private float xRotation = 0f;
 
-    private float mouseSenstivity = 100f;
+    public float mouseSenstivity = 300f;
+
+    // Jump and Gravity
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+    private bool isGround;
+
+    public Transform groundChecker;
+    public float groundCheckerRadius;
+    public LayerMask obstacleLayer;
+
+    public float jumpHeight = 0.1f;
+    public float gravityDivide = 100f;
+    public float jumpSpeed = 10;
+
 
     void Awake()
     {
@@ -25,8 +39,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //Check character is grounded
+        isGround = Physics.CheckSphere(groundChecker.position, groundCheckerRadius, obstacleLayer);
+        
         //Movement
-        Vector3 moveInputs = Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward;
+        Vector3 moveInputs = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         Vector3 moveVelocity = moveInputs * Time.deltaTime * speed;
 
         controller.Move(moveVelocity);
@@ -38,5 +55,24 @@ public class PlayerMovement : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //Jump and Gravity
+
+        if (!isGround)
+        {
+            velocity.y += gravity * Time.deltaTime / gravityDivide;
+            speed = jumpSpeed;
+        }
+        else
+        {
+            velocity.y = -0.05f;
+            speed = 10;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity / gravityDivide);
+        }
+        
+        controller.Move(velocity);
     }
 }
