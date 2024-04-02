c971ceef3697897974326494a8d3d0ecc4443ec9
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2024-04-02 22:48:37 +0300

    Enemy-02
    Added laser that kills our player and added player death animation.

--- Assets/Scripts/LaserEnemy.cs
+++ Assets/Scripts/LaserEnemy.cs
@@ -6,16 +6,29 @@ using UnityEngine;
 public class LaserEnemy : MonoBehaviour
 {
     RaycastHit hit;
-    public LayerMask obstacle;
+    public LayerMask obstacle, player_layer;
+
+    public GameObject death_effect;
 
     private void Update()
     {
         if(Physics.Raycast(transform.position, transform.forward, out hit,Mathf.Infinity, obstacle))
         {
+            GetComponent<LineRenderer>().enabled = true;
             GetComponent<LineRenderer>().SetPosition(0, transform.position);
             GetComponent<LineRenderer>().SetPosition(1,hit.point);
 
             GetComponent<LineRenderer>().startWidth = 0.025f + Mathf.Sin(Time.time) /80;
+        }
+        else
+        {
+            GetComponent<LineRenderer>().enabled = false;
+        }
+
+        //Kill Player
+        if(Physics.Raycast(transform.position, transform.forward,out hit, Mathf.Infinity, player_layer))
+        {
+            hit.transform.gameObject.GetComponent<PlayerManager>().Death();
         }
     }
 }
