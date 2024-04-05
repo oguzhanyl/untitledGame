678a8738d85502a5585a80a69fdf53aa27ebfae9
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2024-04-06 01:04:12 +0300

    Enemy-08
    Player's dead by drone strike

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,6 +8,10 @@ public class Bullet : MonoBehaviour
 
     public float lifetime = 3f;
 
+    public bool enemy_bullet = false;
+    public float bullet_radius = 0.5f;
+    public LayerMask player_layer;
+
 
     private void Update()
     {
@@ -18,6 +22,15 @@ public class Bullet : MonoBehaviour
         if (lifetime <= 0)
         {
             Destroy(this.gameObject);
+        }
+
+        //Enemy Bullet
+        if(enemy_bullet)
+        {
+            if(Physics.CheckSphere(transform.position, bullet_radius, player_layer))
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>().Death();
+            }
         }
     }
 }
